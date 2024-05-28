2d3728b12755cc26e9c44e06cb37ade17299f950
Jules Grelet <jules.grelet@live.fr>
2024-05-28 19:44:18 +0200

    maj

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, 'front/coverage/bobapp'),
+      dir: require('path').join(__dirname, 'fronttt/coverage/bobapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
